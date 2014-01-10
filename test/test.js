a4b87cfaff4a2a1da0ed4031f47b1dbd4061c59a
Sean Noble <sean.noble.72@gmail.com>
2014-01-10 12:08:54 +0100

    Add tests to check text descriptions of hands accurate

--- test/test.js
+++ test/test.js
@@ -149,54 +149,76 @@
         });
 
         describe('get hand values', function () {
+            var handDetails;
+
             it('straight flush should have value [STRAIGHT_FLUSH,5,4,3,2,1]', function () {
                 // 'As 2s 3s 4s 5s'
-                hands['straightFlushes'][0].getHandValue().should.eql([hType.STRAIGHT_FLUSH,5,4,3,2,1]);
+                handDetails = hands['straightFlushes'][0].getHandDetails();
+                handDetails.value.should.eql([hType.STRAIGHT_FLUSH,5,4,3,2,1]);
+                handDetails.name.should.equal('Straight flush, five high');
             });
 
             it('quads should have value [QUADS,2,14]', function () {
                 // '2s 2d 2h 2c Ac'
-                hands['quads'][0].getHandValue().should.eql([hType.QUADS,2,14]);
+                handDetails = hands['quads'][0].getHandDetails();
+                handDetails.value.should.eql([hType.QUADS,2,14]);
+                handDetails.name.should.equal('Four twos');
             });
 
             it('fullhouse should have value [FULL_HOUSE,14,13]', function () {
                 // 'As Ah Ad Ks Kc'
-                hands['fullHouses'][0].getHandValue().should.eql([hType.FULL_HOUSE,14,13]);
+                handDetails = hands['fullHouses'][0].getHandDetails();
+                handDetails.value.should.eql([hType.FULL_HOUSE,14,13]);
+                handDetails.name.should.equal('Fullhouse, aces over kings');
             });
 
             it('flush should have value [FLUSH,14,13,7,6,3]', function () {
                 // '7s 6s As Ks 3s'
-                flush.getHandValue().should.eql([hType.FLUSH,14,13,7,6,3]);
+                handDetails = flush.getHandDetails();
+                handDetails.value.should.eql([hType.FLUSH,14,13,7,6,3]);
+                handDetails.name.should.equal('Flush, ace high');
             });
 
             it('straight should have value [STRAIGHT,7,6,5,4,3]', function () {
                 // '7s 6s 4c 3h 5s'
-                straight.getHandValue().should.eql([hType.STRAIGHT,7,6,5,4,3]);
+                handDetails = straight.getHandDetails();
+                handDetails.value.should.eql([hType.STRAIGHT,7,6,5,4,3]);
+                handDetails.name.should.equal('Straight, seven high');
             });
 
             it('low straight should have value [STRAIGHT,5,4,3,2,1]', function () {
                 // 'As 2s 4c 3h 5s'
-                lowStraight.getHandValue().should.eql([hType.STRAIGHT,5,4,3,2,1]);
+                handDetails = lowStraight.getHandDetails();
+                handDetails.value.should.eql([hType.STRAIGHT,5,4,3,2,1]);
+                handDetails.name.should.equal('Straight, five high');
             });
 
             it('trip aces should have value [TRIPS,14,13,3]', function () {
                 // 'As Ah Ad Ks 3c'
-                tripAces.getHandValue().should.eql([hType.TRIPS,14,13,3]);
+                handDetails = tripAces.getHandDetails();
+                handDetails.value.should.eql([hType.TRIPS,14,13,3]);
+                handDetails.name.should.equal('Three aces');
             });
 
             it('two pair should have value [TWO_PAIR,10,2,11]', function () {
                 // '2s 2c Ts Td Jh'
-                hands['twoPairs'][0].getHandValue().should.eql([hType.TWO_PAIR,10,2,11]);
+                handDetails = hands['twoPairs'][0].getHandDetails();
+                handDetails.value.should.eql([hType.TWO_PAIR,10,2,11]);
+                handDetails.name.should.equal('Two pair, tens over twos');
             });
 
             it('pair should have value [PAIR,14,7,4,3]', function () {
                 // 'As Ad 7c 3s 4d'
-                hands['pairs'][0].getHandValue().should.eql([hType.PAIR,14,7,4,3]);
+                handDetails = hands['pairs'][0].getHandDetails();
+                handDetails.value.should.eql([hType.PAIR,14,7,4,3]);
+                handDetails.name.should.equal('Pair of aces');
             });
 
             it('high card should have value [HIGH_CARD,14,12,9,4,2]', function () {
                 // 'Ad Qh 9s 4c 2h'
-                hands['highCards'][0].getHandValue().should.eql([hType.HIGH_CARD,14,12,9,4,2]);
+                handDetails = hands['highCards'][0].getHandDetails();
+                handDetails.value.should.eql([hType.HIGH_CARD,14,12,9,4,2]);
+                handDetails.name.should.equal('High card ace');
             });
         });
 
@@ -272,7 +294,7 @@
 
     function allSameHandType(hands, expectedHandType) {
         for (var idx = 0; idx < hands.length; idx += 1) {
-            hands[idx].getHandValue()[0].should.equal(expectedHandType, hands[idx]);
+            hands[idx].getHandDetails().value[0].should.equal(expectedHandType, hands[idx]);
         };
     }
 })();
